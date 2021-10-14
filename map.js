087694b520033dc6288d8eebc3ea5410bbc81414
f[nZk] <fncolon@pm.me>
2021-10-14 16:51:04 +0700

    Add `Array.map` code example

---
+++ map.js
@@ -0,0 +1,13 @@
+/**
+* Author : Muhammad Fauzan
+* Array.map - https://github.com/devoverid/learning-resources
+*/
+
+// Mapping is any prescribed way of assigning to each object in one set a particular object in another, based on mathematics theory
+
+const arr = [2, 4, 2, 4];
+
+// map all value inside type of Array object to `value * 4`
+const mapped = arr.map(x => x * 4);
+
+console.log(mapped);
